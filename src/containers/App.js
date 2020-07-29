76d082cfe3f594266bdb7ce84bcb344b9a913a8b
Moazam05 <salmanmoazam08@gmail.com>
2020-07-29 16:34:56 +0500

    Form Validator in ChNoTwo

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import "../containers/App.js"
+
+function App() {
+  return (
+    <div className="App">
+      <h2>React</h2>
+     
+    </div>
+  );
+}
+
+export default App;

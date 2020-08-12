7594918718d15c038c52b8a48af3ff7d0954386c
Moazam05 <salmanmoazam08@gmail.com>
2020-08-12 16:10:37 +0500

    Calculate Wealth - reduce()

--- src/components/ChNoSix/script.js
+++ src/components/ChNoSix/script.js
@@ -55,6 +55,19 @@ function showMillionaires() {
   updateDOM();
 }
 
+// Calculate Total Wealth
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+  const wealthEl = document.createElement('div');
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+    wealth
+  )}</strong> </h3>`;
+  main.appendChild(wealthEl);
+
+  //   updateDOM();
+}
+
 // Update DOM
 function updateDOM(providedData = data) {
   // Clear main div
@@ -80,3 +93,4 @@ addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click', sortByRichest);
 showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);

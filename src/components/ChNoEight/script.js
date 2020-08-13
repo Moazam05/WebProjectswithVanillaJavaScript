b7b4833c8305c4035ea963c86dd579048130a210
Moazam05 <salmanmoazam08@gmail.com>
2020-08-13 14:42:12 +0500

    Letter Press Event Handler

---
+++ src/components/ChNoEight/script.js
@@ -0,0 +1,76 @@
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-again');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['application', 'programming', 'interface', 'wizard', 'love'];
+
+const selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = [];
+const wrongLetters = [];
+
+// Show hidden word
+function displayWord() {
+  wordEl.innerHTML = `
+    ${selectedWord
+      .split('')
+      .map(
+        (letter) => `
+            <span class="letter">
+            ${correctLetters.includes(letter) ? letter : ''}
+            </span>
+            `
+      )
+      .join('')}
+    
+    `;
+  const innerWord = wordEl.innerText.replace(/\n/g, '');
+
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = 'Congratulations! You won! 😀';
+    popup.style.display = 'flex';
+  }
+
+  console.log(wordEl.innerText, innerWord);
+}
+
+// Update the wrong letters
+function updateWrongLettersEl() {
+  console.log('Update wrong');
+}
+
+// Show notification
+function showNotification() {
+  notification.classList.add('show');
+
+  setTimeout(() => {
+    notification.classList.remove('show');
+  }, 2000);
+}
+
+//  Keydow letter press
+window.addEventListener('keydown', (e) => {
+  const letter = e.key;
+  if (selectedWord.includes(letter)) {
+    if (!correctLetters.includes(letter)) {
+      correctLetters.push(letter);
+      displayWord();
+    } else {
+      showNotification();
+    }
+  } else {
+    if (!wrongLetters.includes(letter)) {
+      wrongLetters.push(letter);
+      updateWrongLettersEl();
+    } else {
+      showNotification();
+    }
+  }
+});
+
+displayWord();
